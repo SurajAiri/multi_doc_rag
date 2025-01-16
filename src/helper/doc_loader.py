26d6c0538622d2095095413a689d32c9ff7b2fdb
SurajAiri <surajairi.ml@gmail.com>
2025-01-16 08:30:50 +0530

    pdf-load-bytes

--- src/helper/doc_loader.py
+++ src/helper/doc_loader.py
@@ -1,6 +1,7 @@
 from langchain.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
+from pathlib import Path
 
 class DocLoader:
     @staticmethod
@@ -18,6 +19,44 @@ class DocLoader:
             return DocLoader.__split_docs__(docs)
         except Exception as e:
             raise RuntimeError(f"An error occurred while loading the PDF: {e}")
+    
+    @staticmethod
+    def load_pdf_bytes(name, pdf_bytes):
+        try:
+            # id = uuid.uuid4()
+            OUTPUT_DIR = "output/upload/pdf/"
+            Path(OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
+            file_path = os.path.join(OUTPUT_DIR, name)
+
+            if os.path.exists(file_path):
+                raise FileExistsError(f"The file at path {file_path} already exists.")
+            
+            
+            # save the pdf file
+            with open(file_path, "wb") as f:
+                f.write(pdf_bytes)
+            
+            # loading file
+            pdf = PyMuPDFLoader(file_path)
+            docs = pdf.load()
+
+            # Splitting the document into chunks
+            return DocLoader.__split_docs__(docs)
+        except FileExistsError as e:
+            raise e
+        except Exception as e:
+            raise RuntimeError(f"An error occurred while loading the PDF: {e}")
+
+    # Todo: Check this approach later for the implementation with langchain  
+    # # Read the PDF from bytes
+    # import fitz  # PyMuPDF
+    # pdf_document = fitz.open(stream=uploaded_file.getvalue(), filetype="pdf")
+
+    # # Extract text from each page
+    # text = ""
+    # for page_num in range(len(pdf_document)):
+    #     page = pdf_document.load_page(page_num)
+    #     text += page.get_text()
 
     @staticmethod
     def __split_docs__(docs):
